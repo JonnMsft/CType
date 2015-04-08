63be85f11fbb1812accf271570a3943da9614615
JonnMsft <jonn_msft@hotmail.com>
2015-04-07 23:08:33 -0700

    Work around Uninstall-ChocolateyPackage.ps1 bug

--- chocolateyUninstall.ps1
+++ chocolateyUninstall.ps1
@@ -1,27 +1,21 @@
-try {
-    $dir = Split-Path -Parent $MyInvocation.MyCommand.Path
-    $path = Join-Path $dir 'tools\CType.msi'
-    if (Test-Path $path)
-    {
-        Write-Debug "found $path"
-    }
-    else
-    {
-        Write-Debug "did not find $path"
-    }
-    Write-Debug "CType\chocolateyUninstall.ps1 running Uninstall-ChocolateyPackage -PackageName CType -FileType MSI -SilentArgs /Quiet -File $path"
-    Uninstall-ChocolateyPackage -PackageName CType -FileType MSI -SilentArgs '/Quiet' -File $path
-    Write-Debug "CType\chocolateyInstall.ps1 ran MSI uninstaller"
-    $expectedPath = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules\CType"
-    if (Test-Path $expectedPath)
-    {
-        Write-ChocolateyFailure 'CType' "MSI succeeded but $expectedPath is still present"
-    }
-    else
-    {
-        Write-ChocolateySuccess 'CType'
-    }
-} catch {
-    Write-ChocolateyFailure 'CType' $_.Exception.Message
-    throw 
+$dir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$path = Join-Path $dir 'tools\CType.msi'
+if (Test-Path $path)
+{
+    Write-Debug "CType\chocolateyUninstall.ps1: found $path"
 }
+    else
+{
+    Write-Debug "CType\chocolateyUninstall.ps1: did not find $path"
+}
+
+$env:chocolateyInstallOverride = "true"
+$env:chocolateyInstallArguments = "$path /quiet"
+Write-Debug "The current version of Chocolatey appears to contain a bug in Uninstall-ChocolateyPackage,"
+Write-Debug "where the -File parameter value is ignored when -FileType is MSI."
+Write-Debug "I bypass this by setting"
+Write-Debug '$env:chocolateyInstallOverride = "true"'
+Write-Debug '$env:chocolateyInstallArguments = "$path /quiet"'
+Write-Debug "CType\chocolateyUninstall.ps1: running Uninstall-ChocolateyPackage -PackageName CType -FileType MSI -SilentArgs -File $path"
+Uninstall-ChocolateyPackage -PackageName CType -FileType MSI -SilentArgs '/Quiet' -File $path
+Write-Debug "CType\chocolateyInstall.ps1 ran MSI uninstaller"
